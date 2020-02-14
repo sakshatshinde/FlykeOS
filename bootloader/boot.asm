57cbc8f79698565cad7c3f7f34a6f844afb5dad1
sakshatshinde <shindesakshat@gmail.com>
2020-02-14 11:47:12 +0530

    Bootloader now enters 32 bit mode

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -18,11 +18,11 @@ mov dh, 2         ; read 2 sectors
 
 call LOAD_DISK             ; Loading in the disk // boot_disk.asm
 
-mov dx, [0x9000]           ; retrieve the first loaded word, 0xdada
+mov dx, [0x9000]           ; retrieve the first loaded word, 0x0002 //ZeroTwo
 call BIOS_PRINT_HEX_FUNC
 ;call BIOS_PRINT_FUNC      ; uncomment for looking at the ASCII
 
-mov dx, [0x9000 + 512]     ; first word from second loaded sector, 0xface
+mov dx, [0x9000 + 512]     ; first word from second loaded sector, 0x0016 //Hiro
 ;call BIOS_PRINT_FUNC      ; uncomment for looking at the ASCII
 call BIOS_PRINT_HEX_FUNC
 
@@ -35,14 +35,16 @@ jmp INFINITE_LOOP
  
 %include "boot_print_msg.asm"
 %include "boot_disk.asm"
+%include "boot_print.asm"
 %include "boot_print_hex.asm"
+
  
    times 510-($-$$) db 0      ; Padding the magic number with 0's
    db 0x55                    ; Magic
    db 0xAA                    ; Number
 
-   times 256 dw 0xdada        ; sector 2 = 512 bytes
-   times 256 dw 0xface        ; sector 3 = 512 bytes
+   times 256 dw 0x0002        ; sector 2 = 512 bytes // ZeroTwo reference
+   times 256 dw 0x0016        ; sector 3 = 512 bytes // Code 016 reference 
 
    ; We know that BIOS will load only the first 512 - byte sector from the disk ,
    ; so if we purposely add a few more sectors to our code by repeating some
